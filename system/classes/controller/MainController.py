e41ba24568f423258dca168a05d36f8e5c3898ff
wsbrito <wsbrito1975@gmail.com>
2020-08-29 16:50:18 -0300

    Versão com todos as operações mapeadas até o momento expostas no swagger da aplicação. Mudanças a serem submetidas: 	modified:   classes/controller/MainController.py 	modified:   swagger.yml

--- system/classes/controller/MainController.py
+++ system/classes/controller/MainController.py
@@ -54,3 +54,100 @@ def deletar_carro(id):
     :return: 
     '''
     return make_response('Funcionalidade indisponível',404)
+
+def listar_carros_disponiveis():
+    '''
+    GET api/carro/disponiveis
+    Metodo para recuperar todos os carros disponiveis para locacao
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def listar_clientes():
+    '''
+    GET api/cliente
+    Metodo para recuperar todos os clientes da base de dados
+    :return: Lista de clientes
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def incluir_cliente(cliente):
+    '''
+    POST api/cliente
+    Metodo para realizar a inclusao do cliente no sistema
+    :param cliente:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def atualizar_cliente(cliente):
+    '''
+    PUT api/cliente
+    Metodo para realizar a atualizacao do cliente no sistema
+    :param cliente:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def get_cliente(id):
+    '''
+    GET api/cliente/{id}
+    Metodo para recuperar o cliente do id passado como parametro
+    :param id:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def excluir_cliente(id):
+    '''
+    DELETE api/cliente/{id}
+    Metodo para realizar a excluisao do cliente no sistema
+    :param id:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def incluir_locacao(locacao):
+    '''
+    POST api/locacao
+    Metodo para realizar a inclusao da locacao no sistema
+    :param locacao:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def atualizar_locacao(locacao):
+    '''
+    PUT api/locacao
+    Metodo para atualizar a locacao no sistema
+    :param locacao:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def get_locacao_por_id(id):
+    '''
+    GET api/locacao/{id}
+    Metodo para recuperar a locacao pelo seu id.
+    :param id:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def calcular_locacao(id):
+    '''
+    GET api/locacao/calculo/{id}
+    Metodo para realizar o calculo monetario da locacao
+    :param id:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
+
+def get_locacao_por_cpf_ou_placa(cpf_placa):
+    '''
+    GET api/locacao/cpf-placa/{cpf_placa}
+    Metodo para recuperar a locacao com base no CPF ou na PLACA do carro.
+    :param cpf_placa:
+    :return:
+    '''
+    return make_response('Funcionalidade indisponível',404)
